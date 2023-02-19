aeac4d0f513918f935de7c856797d8995a6fcd02
Kirils Aleksejevs <102963681+letseatmeat@users.noreply.github.com>
2023-02-19 13:05:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ def find_mismatch(text):
         if next_char in ")]}":
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i + 1
-                opening_brackets_stack.pop()
-            if opening_brackets_stack:
-                return opening_brackets_stack[0].position
+            opening_brackets_stack.pop()
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
             
-        return "Success"
+    return "Success"
 
 
 def main():
